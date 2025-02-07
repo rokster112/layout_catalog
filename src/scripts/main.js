b951dd912550ca0a7a5438b10bc71d2e1ec4a587
rokas <rokster112@gmail.com>
2025-02-07 17:22:20 +0200

    it shows that all tests have been passed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+async function loadCard() {
+  const response = await fetch("card.html");
+  const cardHTML = await response.text();
+  document.getElementsByClassName("card-div").innerHTML = cardHTML.repeat(3); // Inserts the card 3 times
+}
+
+loadCard();
